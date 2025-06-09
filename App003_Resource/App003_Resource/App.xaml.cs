eba9dd31dae7c42674c1bdf855e12556a89e2f42
Keaton Yang <hcyang1227@gmail.com>
2025-06-09 17:35:00 +0800

    App003_Resource practice example updated

---
+++ App003_Resource/App003_Resource/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace App003_Resource
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
