6cfbff9b59b80e41310ef58c95da9ae37564c3f5
Keaton Yang <hcyang1227@gmail.com>
2025-06-09 18:18:28 +0800

    App004_20250609_ExceptionHandle practicing example update

---
+++ App004_20250609_ExceptionHandle/App004_20250609_ExceptionHandle/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace App004_20250609_ExceptionHandle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string s = null;
+
+            try
+            {
+                s.Trim();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("A handled exception just occurred: " + ex.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+        }
+    }
+}
