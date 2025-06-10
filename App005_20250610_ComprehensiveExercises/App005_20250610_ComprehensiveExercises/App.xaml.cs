c4951fbd3b2f7f77a29294b8b9af861800cf584f
Keaton Yang <hcyang1227@gmail.com>
2025-06-10 14:55:47 +0800

    App005_20250610_ComprehensiveExercises Ver.01 updated

---
+++ App005_20250610_ComprehensiveExercises/App005_20250610_ComprehensiveExercises/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace App005_20250610_ComprehensiveExercises
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ App005_20250610_ComprehensiveExercises/App005_20250610_ComprehensiveExercises/Information.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace App005_20250610_ComprehensiveExercises
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            //Save panel info to dictionary
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            dict.Add("FirstName", txtFName.Text);
+            dict.Add("LastName", txtLName.Text);
+            dict.Add("StreetName", txtStreet.Text);
+            dict.Add("ZipCode", txtZip.Text);
+            dict.Add("City", cbbCity.Text);
+
+            //Show input result to messagebox
+            string fullContent = "";
+            foreach (string str in dict.Keys) { fullContent += str + ": " + dict[str] + "\n"; }
+            MessageBox.Show(fullContent, "Input Information", MessageBoxButton.OKCancel, MessageBoxImage.Information);
+        }
+    }
+}
