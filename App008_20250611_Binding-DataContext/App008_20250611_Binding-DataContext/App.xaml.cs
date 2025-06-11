bb0aa37b65f80d2bb06286c4e9c7eb0dcdc3b098
Keaton Yang <hcyang1227@gmail.com>
2025-06-11 15:06:14 +0800

    App008_20250611_Binding-DataContext practicing example update

---
+++ App008_20250611_Binding-DataContext/App008_20250611_Binding-DataContext/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace App008_20250611_Binding_DataContext
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
