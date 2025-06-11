2da0b6f9ef5d9e8a17ffcc07c6a9199b82d04e9a
Keaton Yang <hcyang1227@gmail.com>
2025-06-11 17:26:00 +0800

    App010_20250611_Binding-UpdateSourceTrigger practicing example update

---
+++ App010_20250611_Binding-UpdateSourceTrigger/App010_20250611_Binding-UpdateSourceTrigger/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace App010_20250611_Binding_UpdateSourceTrigger
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
