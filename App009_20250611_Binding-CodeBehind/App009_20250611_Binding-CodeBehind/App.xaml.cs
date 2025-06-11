409d7e2cd88e861b948db0df2a6bb786b71015b8
Keaton Yang <hcyang1227@gmail.com>
2025-06-11 16:52:37 +0800

    App009_20250611_Binding-CodeBehind practicing example update

---
+++ App009_20250611_Binding-CodeBehind/App009_20250611_Binding-CodeBehind/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace App009_20250611_Binding_CodeBehind
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
